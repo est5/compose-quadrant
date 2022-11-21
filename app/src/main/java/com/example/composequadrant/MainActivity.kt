b880927812b9d025ebd387826504f4fa2060eb1c
est5 <ghostrekon5500@ya.ru>
2022-11-21 22:44:26 +0300

    donE

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +25,31 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeQuadrantTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
+                Surface(
                     color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Column(Modifier.fillMaxWidth()) {
+                        Row(Modifier.weight(1f)) {
+                            ColoredBox(color = Color.Green,
+                                title = stringResource(R.string.green_title),
+                                paragraph = stringResource(R.string.green_para),
+                                modifier = Modifier.weight(1f))
+                            ColoredBox(color = Color.Yellow,
+                                title = stringResource(R.string.yellow_title),
+                                paragraph = stringResource(R.string.yellow_para),
+                                modifier = Modifier.weight(1f))
+
+                        }
+                        Row(Modifier.weight(1f)) {
+                            ColoredBox(color = Color.Cyan,
+                                title = stringResource(R.string.cyan_title),
+                                paragraph = stringResource(R.string.cyan_para),
+                                modifier = Modifier.weight(1f))
+                            ColoredBox(color = Color.LightGray,
+                                title = stringResource(R.string.gray_title),
+                                paragraph = stringResource(R.string.gray_para),
+                                modifier = Modifier.weight(1f))
+                        }
+                    }
                 }
             }
         }
@@ -28,14 +57,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ColoredBox(color: Color, title: String, paragraph: String, modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(color),
+
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = title,
+                modifier = Modifier
+                    .padding(bottom = 16.dp),
+                fontWeight = FontWeight(700),
+            )
+            Text(text = paragraph,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(16.dp))
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        Column() {
+            Row {
+                ColoredBox(color = Color.Green,
+                    title = stringResource(R.string.green_title),
+                    paragraph = stringResource(R.string.green_para))
+                ColoredBox(color = Color.Yellow,
+                    title = stringResource(R.string.yellow_title),
+                    paragraph = stringResource(R.string.yellow_para))
+            }
+            Row {
+                ColoredBox(color = Color.Cyan,
+                    title = stringResource(R.string.cyan_title),
+                    paragraph = stringResource(R.string.cyan_para))
+                ColoredBox(color = Color.LightGray,
+                    title = stringResource(R.string.gray_title),
+                    paragraph = stringResource(R.string.gray_para))
+            }
+        }
     }
 }
